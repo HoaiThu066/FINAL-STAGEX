bb371220fdd0aaa79d9b70c0e5b21d7616daedd0
trangle- 1509 <mytrangle1509@gmail.com>
2025-11-06 00:44:32 +0700

    Config -mail

---
+++ config/mail.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'host' => 'smtp.gmail.com',      
+    'username' => 'dtngoc.video@gmail.com',  
+    'password' => 'yfdc ojad kfbl argt',    
+    'port' => 587,              
+    'encryption' => 'tls',        
+    'from_email' => 'no-reply@stagex.local',
+    'from_name' => 'StageX'
+];
