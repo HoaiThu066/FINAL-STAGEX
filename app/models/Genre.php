1c5e5d64dddc598e938ea3da1b714aad5b2a8951
Thuy Trang <trangnguyen.31231026201@st.ueh.edu.vn>
2025-11-07 20:10:14 +0700

    sua model genre

--- app/models/Genre.php
+++ app/models/Genre.php
@@ -3,14 +3,8 @@ namespace App\Models;
 
 class Genre extends Database
 {
-    /**
-     * 
-     *
-     * @return array
-     */
     public function all(): array
     {
-        
         $pdo = $this->getConnection();
         try {
             $stmt = $pdo->query('CALL proc_get_all_genres()');
@@ -22,20 +16,11 @@ class Genre extends Database
         }
     }
 
-
-    /**
-     * 
-     *
-     * @param string $name Name of the genre
-     * @return bool True on success, false on failure
-     */
     public function create(string $name): bool
     {
-        
         try {
             $stmt = $this->getConnection()->prepare('CALL proc_create_genre(:name)');
             $stmt->execute(['name' => $name]);
-           
             $stmt->fetch();
             $stmt->closeCursor();
             return true;
@@ -44,16 +29,8 @@ class Genre extends Database
         }
     }
 
-
-    /**
-    
-     *
-     * @param int $id
-     * @return bool
-     */
     public function delete(int $id): bool
     {
-        
         try {
             $stmt = $this->getConnection()->prepare('CALL proc_delete_genre(:id)');
             $stmt->execute(['id' => $id]);
@@ -64,17 +41,8 @@ class Genre extends Database
         }
     }
 
-
-    /**
-     * 
-     *
-     * @param int    $id   Genre identifier
-     * @param string $name New genre name
-     * @return bool        True on success
-     */
     public function update(int $id, string $name): bool
     {
-       
         try {
             $stmt = $this->getConnection()->prepare('CALL proc_update_genre(:id, :name)');
             $stmt->execute([
@@ -88,4 +56,10 @@ class Genre extends Database
         }
     }
 }
+
+
+
+
+
+
 
